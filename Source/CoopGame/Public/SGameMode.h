c02a33120e68732416913119dcda852f2cc08bed
DESKTOP-96GJUGK\antho <anthony.nicolosi78@gmail.com>
2018-02-21 19:40:31 -0700

    Implemented new wave-state related functions in SGameMode.  Implemented SpawnNewBot in BP_TestGameMode.
    Lecture 121

--- Source/CoopGame/Public/SGameMode.h
+++ Source/CoopGame/Public/SGameMode.h
@@ -6,15 +6,44 @@
 #include "GameFramework/GameModeBase.h"
 #include "SGameMode.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class COOPGAME_API ASGameMode : public AGameModeBase
 {
 	GENERATED_BODY()
 	
-	
-	
+public:
+	virtual void StartPlay() override;
+	ASGameMode();
+
+protected:
+	int32 WaveCount;
+	int32 NumBotsToSpawnInCurrentWave;
+	FTimerHandle TimerHandle_BotSpawner;
+
+	UPROPERTY(EditDefaultsOnly, Category = "GameMode")
+	float TimeBetweenWaves;
+
+	/*
+	Hook for BP to spawn a single bot
+	*/
+	UFUNCTION(BlueprintImplementableEvent, Category = "GameMode")
+	void SpawnNewBot();
+
+	void SpawnBotTimerElapsed();
+
+	/*
+	Start spawning bots
+	*/
+	void StartWave();
+
+	/*
+	Stop spawning bots
+	*/
+	void EndWave();
+
+	/*
+	Set timer for next startwave
+	*/
+	void PrepareForNextWave();
 	
 };
